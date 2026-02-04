340ee6ac711751abf02beda7ebeeaf387016d904
stancld <daniel.stancl@gmail.com>
2026-02-04 15:29:37 +0100

    rossum-agent: Release rossum-agent 1.0.0rc4

--- rossum-agent/rossum_agent/__init__.py
+++ rossum-agent/rossum_agent/__init__.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 
 from rossum_agent.agent import AgentConfig, AgentStep, RossumAgent, create_agent
 
-__version__ = "1.0.0rc3"
+__version__ = "1.0.0rc4"
 
 __all__ = ["AgentConfig", "AgentStep", "RossumAgent", "create_agent"]
