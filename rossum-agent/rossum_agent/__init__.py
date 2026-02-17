8300673c0645c743aeae718902a1aa74315f697d
stancld <daniel.stancl@gmail.com>
2026-02-17 14:32:24 +0100

    rossum-agent: Start 1.3.0

--- rossum-agent/rossum_agent/__init__.py
+++ rossum-agent/rossum_agent/__init__.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 
 from rossum_agent.agent import AgentConfig, AgentStep, RossumAgent, create_agent
 
-__version__ = "1.2.0"
+__version__ = "1.3.0dev"
 
 __all__ = ["AgentConfig", "AgentStep", "RossumAgent", "create_agent"]
